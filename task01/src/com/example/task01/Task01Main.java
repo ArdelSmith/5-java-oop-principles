50e928f60ed86da89575c0cb87b63b7db2a92833
Ivan Danilyuk <werwetsad@gmail.com>
2022-11-05 21:56:11 +0500

    Completed Task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,14 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
+        Logger l1 = Logger.getLogger("test");
+        Logger l2 = Logger.getLogger("test");
+        l2.setName("345");
+        l2.setLevel(Logger.Level.WARNING);
+        l2.debug("123");
+        l2.info("123");
+        l2.warning("bebra occured");
+        l2.error("123");
 
     }
 }
