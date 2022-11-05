7d4bd1a2251d54fbebf950500b73566d5b5d3ec0
Ivan Danilyuk <werwetsad@gmail.com>
2022-11-05 22:16:19 +0500

    Completed Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,20 +2,17 @@ package com.example.task02;
 
 public class Task02Main {
 
-    private static final Item ITEM1 = new Item("Товар 1", 10);
-    private static final Item ITEM2 = new Item("Товар 2", 20);
-    private static final Item ITEM3 = new Item("Товар 3", 30);
-    private static final Item ITEM4 = new Item("Товар 4", 40);
-    private static final Item ITEM5 = new Item("Товар 5", 50);
-    private static final Item ITEM6 = new Item("Товар 6", 60);
+    private static final Item ITEM1 = new Item("Product 1", 10);
+    private static final Item ITEM2 = new Item("Product 2", 20);
+    private static final Item ITEM3 = new Item("Product 3", 30);
+    private static final Item ITEM4 = new Item("Product 4", 40);
+    private static final Item ITEM5 = new Item("Product 5", 50);
+    private static final Item ITEM6 = new Item("Product 6", 60);
 
     public static void main(String[] args) {
-        Bill bill = new Bill();
-        bill.add(ITEM1, 10);
-        bill.add(ITEM3, 3);
-        bill.add(ITEM6, 1);
-        System.out.println(bill);
-        bill.add(ITEM3, 3);
-        System.out.println(bill);
+        DiscountBill bl = new DiscountBill(50);
+        bl.add(ITEM5, 3);
+        System.out.println(bl.getPrice());
+        System.out.println(bl.getAbsoluteDiscount());
     }
 }
